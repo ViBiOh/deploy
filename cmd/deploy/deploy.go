1e946d9d7e01fc55941b8056a470b2e4b73b639e
Vincent Boutour <bob@vibioh.fr>
2021-06-03 19:45:54 +0200

    refactor: Harmonizing pprof handler position
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -59,9 +59,9 @@ func main() {
 
 	go apiApp.Start(healthApp.Done())
 
+	go pprofServer.Start("pprof", healthApp.End(), http.DefaultServeMux)
 	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
 	go appServer.Start("http", healthApp.End(), httputils.Handler(apiApp.Handler(), healthApp, recoverer.Middleware, prometheusApp.Middleware, owasp.New(owaspConfig).Middleware))
-	go pprofServer.Start("pprof", healthApp.End(), http.DefaultServeMux)
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done(), promServer.Done())
