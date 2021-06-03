f1dba7b18803cb900de53ddbbe1bd4bd6f6e0fe2
Vincent Boutour <bob@vibioh.fr>
2021-06-03 19:13:29 +0200

    feat: Adding pprof handler
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 
 	"github.com/ViBiOh/deploy/pkg/annotation"
@@ -24,6 +26,7 @@ func main() {
 
 	appServerConfig := server.Flags(fs, "", flags.NewOverride("WriteTimeout", "2m"))
 	promServerConfig := server.Flags(fs, "prometheus", flags.NewOverride("Port", 9090), flags.NewOverride("IdleTimeout", "10s"), flags.NewOverride("ShutdownTimeout", "5s"))
+	pprofServerConfig := server.Flags(fs, "pprof", flags.NewOverride("Port", 9999))
 	healthConfig := health.Flags(fs, "")
 
 	alcotestConfig := alcotest.Flags(fs, "")
@@ -43,6 +46,7 @@ func main() {
 
 	appServer := server.New(appServerConfig)
 	promServer := server.New(promServerConfig)
+	pprofServer := server.New(pprofServerConfig)
 	prometheusApp := prometheus.New(prometheusConfig)
 	healthApp := health.New(healthConfig)
 
@@ -57,6 +61,7 @@ func main() {
 
 	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
 	go appServer.Start("http", healthApp.End(), httputils.Handler(apiApp.Handler(), healthApp, recoverer.Middleware, prometheusApp.Middleware, owasp.New(owaspConfig).Middleware))
+	go pprofServer.Start("pprof", healthApp.End(), http.DefaultServeMux)
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done(), promServer.Done())
